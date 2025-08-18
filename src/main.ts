5ccb8d9dc5f47fa4be81fe379af2eb0fee844e4b
vurses <vurses@qq.com>
2025-08-18 20:15:08 +0800

    chore: 随机DedeUserID

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import runScript from "@/core";
 import toggleIntercept from "@/utils/websocket/intercept";
+import { random } from "lodash-es";
 
 // 修改ws连接成功后的发送的第一个数据包以在直播页获取弹幕
-if(location.href.includes('live.bilibili.com')){
-  toggleIntercept()
+if (location.href.includes("live.bilibili.com")) {
+  toggleIntercept();
 }
 // Prevent the player from retrieving the correct playback information
 Object.defineProperty(window, "__playinfo__", {
@@ -15,6 +16,6 @@ Object.defineProperty(window, "__playinfo__", {
 
 // player判断用户登录的另一种方式，用于首次获取何种质量的视频cdn
 // @ts-ignore
-document.cookie = "DedeUserID=3493281916783362";
+document.cookie = `DedeUserID=${random(2 ** 53)}`;
 
 runScript();
