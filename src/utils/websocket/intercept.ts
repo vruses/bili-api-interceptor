a7a7cdd8f9e43e11bc9a96343c98b56956bb8b24
vurses <vurses@qq.com>
2026-01-30 17:44:33 +0800

    chore(ws-intercept): wrap websocket intercept as useWsIntercept composable function

--- src/utils/websocket/intercept.ts
+++ src/utils/websocket/intercept.ts
@@ -1,62 +1,4 @@
-let isInterceptActive: boolean = false
-let wsOriginalSend: ((this: WebSocket, data: string | ArrayBufferLike | Blob | ArrayBufferView) => void) | null = null
-
-/** 修改ws连接成功后的发送的第一个数据包以在直播页获取弹幕 */
-function toggleIntercept(): void {
-  if (!isInterceptActive) {
-    // 启动拦截
-    wsOriginalSend = WebSocket.prototype.send
-    isInterceptActive = true
-
-    WebSocket.prototype.send = function (data: string | ArrayBufferLike | Blob | ArrayBufferView): unknown {
-      if (data instanceof ArrayBuffer) {
-        const view: DataView = new DataView(data)
-
-        // 检查标识为7的数据包
-        if (view.getUint32(8, false) === 7) {
-          // 修改uid逻辑
-          return wsOriginalSend!.call(this, modifyUidPacket(data))
-        }
-      }
-      return wsOriginalSend!.call(this, data)
-    }
-  } else {
-    // 停止拦截
-    if (wsOriginalSend) {
-      WebSocket.prototype.send = wsOriginalSend
-    }
-    isInterceptActive = false
-    wsOriginalSend = null
-  }
-}
-
-// uid修改
-function modifyUidPacket(data: ArrayBuffer): ArrayBuffer {
-  const newBuffer: ArrayBuffer = data.slice()
-  const jsonStart: number = 16
-  const jsonBytes: Uint8Array = new Uint8Array(newBuffer, jsonStart)
-
-  const jsonString: string = new TextDecoder().decode(jsonBytes)
-  const jsonObj: PacketData = JSON.parse(jsonString)
-
-  jsonObj.uid = 0 // 修改uid
-
-  const newJsonString: string = JSON.stringify(jsonObj)
-  const newJsonBytes: Uint8Array = new TextEncoder().encode(newJsonString)
-
-  const newPacket: ArrayBuffer = new ArrayBuffer(16 + newJsonBytes.length)
-  const newView: DataView = new DataView(newPacket)
-
-  // 复制包头并更新长度
-  const originalView: DataView = new DataView(data)
-  for (let i: number = 0; i < 16; i++) {
-    newView.setUint8(i, originalView.getUint8(i))
-  }
-  newView.setUint32(0, newJsonBytes.length + 16, false)
-
-  new Uint8Array(newPacket, 16).set(newJsonBytes)
-  return newPacket
-}
+type WsSend = (this: WebSocket, data: string | ArrayBufferLike | Blob | ArrayBufferView) => void
 
 interface PacketData {
   uid: number
@@ -71,4 +13,60 @@ interface PacketData {
   key: string
 }
 
-export default toggleIntercept
+/** 修改ws连接成功后的发送的第一个数据包以在直播页获取弹幕 */
+export function useWsIntercept() {
+  let wsOriginalSend: WsSend | null = null
+
+  function modifyUidPacket(data: ArrayBuffer): ArrayBuffer {
+    const jsonStart = 16
+    const jsonBytes = new Uint8Array(data, jsonStart)
+    const jsonString = new TextDecoder().decode(jsonBytes)
+    const jsonObj: PacketData = JSON.parse(jsonString)
+
+    jsonObj.uid = 0
+
+    const newJsonBytes = new TextEncoder().encode(JSON.stringify(jsonObj))
+    const newPacket = new ArrayBuffer(16 + newJsonBytes.length)
+    const newView = new DataView(newPacket)
+    const originalView = new DataView(data)
+
+    for (let i = 0; i < 16; i++) {
+      newView.setUint8(i, originalView.getUint8(i))
+    }
+
+    newView.setUint32(0, newJsonBytes.length + 16, false)
+    new Uint8Array(newPacket, 16).set(newJsonBytes)
+
+    return newPacket
+  }
+
+  function start(): void {
+    if (wsOriginalSend) return
+
+    wsOriginalSend = WebSocket.prototype.send
+
+    WebSocket.prototype.send = function (data: string | ArrayBufferLike | Blob | ArrayBufferView): unknown {
+      if (data instanceof ArrayBuffer) {
+        const view = new DataView(data)
+
+        if (view.getUint32(8, false) === 7) {
+          return wsOriginalSend!.call(this, modifyUidPacket(data))
+        }
+      }
+      return wsOriginalSend!.call(this, data)
+    }
+  }
+
+  function stop(): void {
+    if (!wsOriginalSend) return
+
+    WebSocket.prototype.send = wsOriginalSend
+    wsOriginalSend = null
+  }
+
+  return { start, stop }
+}
+
+const { start, stop } = useWsIntercept()
+
+export { start, stop }
